9d3186787911102a77c9266edd7417209d1614f5
junios-study <31557850+junios-study@users.noreply.github.com>
2025-04-08 15:55:34 +0900

    구조체

--- main.cpp
+++ main.cpp
@@ -1,123 +1,42 @@
 #include <iostream>
-#include <vector>
+#include <windows.h>
 
 using namespace std;
 
-void Initialize();
-void Print();
-void Shuffle();
 
-int Pockets[45] = { 0, };
-
-void B()
+void GotoXY(int X, int Y)
 {
-	memset(Pockets, 0, sizeof(Pockets));
+    COORD Cur;
+    Cur.X = X;
+    Cur.Y = Y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
 }
 
-
-void Swap(int* PA, int* PB)
+//custom 자료형
+struct PlayerInfo
 {
-	int Temp = *PA;
-	*PA = *PB;
-	*PB = Temp;
-}
-
-void Swap2(int* PA, int* PB)
-{
-	int* Temp = PA;
-	PA = PB;
-	PB = Temp;
-}
-
-
-int* Resize(int* Original, int OriginalSize, int NewSize)
-{
-	int* NewDynamicArray = new int[NewSize];
-	for (int i = 0; i < OriginalSize; ++i)
-	{
-		NewDynamicArray[i] = Original[i];
-	}
-
-	delete[] Original;
-	Original = nullptr;
-
-	Original = NewDynamicArray;
-	NewDynamicArray = nullptr;
-
-	return Original;
-}
+    int X; 
+    int Y;
+    string Shape;
+};
 
 
 
 int main()
 {
-	int A;
-	inrt
+    PlayerInfo* PlayerData;
+    PlayerData = new PlayerInfo;
 
-	int* DynamicArray = new int[10000];
+///    (*(PlayerData+0)).X = 5;
+    PlayerData->X = 5;
+    PlayerData->Y = 5;
+    PlayerData->Shape = "P";
 
-	for (int i = 0; i < 10; ++i)
-	{
-		DynamicArray[i] = (i + 1) * 3;
-	}
+    GotoXY(PlayerData->X, PlayerData->Y);
 
-	//추가 삭제 비용 적게 드는거
-	//접근 빠르다. 접근에 상수 시간(vector)
-	DynamicArray = Resize(DynamicArray, 10000, 10001);
+    cout << PlayerData->Shape << endl;
 
-	for (int i = 10; i < 20; ++i)
-	{
-		DynamicArray[i] = (i - 9) * 4;
-	}
+    delete PlayerData;
+    PlayerData = nullptr;
 
-
-	for (int i = 0; i < 20; ++i)
-	{
-		cout << DynamicArray[i] << endl;
-	}
-
-	return 0;
-}
-
-
-
-
-
-
-
-
-
-void Initialize()
-{
-	srand(time(NULL));
-
-	for (int i = 0; i < 45; ++i)
-	{
-		Pockets[i] = i + 1;
-	}
-}
-
-
-
-void Shuffle()
-{
-	for (int i = 0; i < 45 * 1000; ++i)
-	{
-		int First = rand() % 45;
-		int Second = rand() % 45;
-
-		int Temp = Pockets[First];
-		Pockets[First] = Pockets[Second];
-		Pockets[Second] = Temp;
-	}
-
-}
-
-
-void Print()
-{
-	for (int i = 0; i < 6; ++i)
-	{
-		cout << Pockets[i] << endl;
-	}
 }
